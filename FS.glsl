1882e819d0134e2d21264d9b94e6346e0277ece2
Fouad Elamoury <foad.osama@hotmail.com>
2024-11-27 21:13:19 +0200

    It's a normal triangle

--- FS.glsl
+++ FS.glsl
@@ -4,5 +4,5 @@ in vec4 frag_color;
 
 void main()
 {
-	gl_FragColor = frag_color;
+	gl_FragColor = vec4(.6f,.5f,0.0f,1.0f);
 }
--- Source.cpp
+++ Source.cpp
@@ -2,218 +2,108 @@
 #include <gl/glew/glew.h>
 #include <SFML/Graphics.hpp>
 #include <SFML/OpenGL.hpp>
-#include<gl\glm\glm.hpp>
+#include <gl/glm/glm.hpp>
+#include <vector>
+#include <cstdlib>
 
 using namespace std;
 using namespace glm;
 
-enum DrawingMode
-{
-	Points,
-	Lines,
-	FilledTriangle
-};
+const GLint WIDTH = 900, HEIGHT = 800;
+GLuint VBO, BasiceprogramId;
 
-struct Vertex
-{
-	vec3 Position;
-	vec3 COlor;
-};
+vector<vec3> positions;
 
 GLuint InitShader(const char* vertex_shader_file_name, const char* fragment_shader_file_name);
 
-const GLint WIDTH = 800, HEIGHT = 800;
-GLuint VBO, BasiceprogramId;
-DrawingMode Current_DrawingMode = DrawingMode::FilledTriangle;
-float triangleSize = 0;
-void CreateTriangle()
-{
-	GLfloat TriangleVertices[180];
-	//=
-	//{
-	//	0,0,0,
-	//	0,0,0,
-	//	0,0,0,
-	//	0,0,0,
-	//	0,0,0,
-	//	0,0,0
-	//	/*
-	/*	-1,-1,0,
-		1,-1,0,
-		1,0,0,
-		1,1,0,
-		0,1,0,
-		-1,1,0*/
-	//	-2,1,0,
-	//	-2,0,0,
-	//	-2,-2,0,
-	//	-1,-3,0,
-	//	1,-3,0,
-	//	1,-1,0,
-	//	-1,0,0*/
-	//};
-	triangleSize = (sizeof(TriangleVertices) / 12);
-	TriangleVertices[0] = 0;
-	TriangleVertices[1] = 0;
-	TriangleVertices[2] = 0;
-	for (int i = 3; i < triangleSize * 3; i += 3)
-	{
-		TriangleVertices[i] = cos((2 * 3.14 * (i - 3)) / ((triangleSize * 3)-6));
-		TriangleVertices[i + 1] = sin((2 * 3.14 * (i - 3)) / ((triangleSize * 3) - 6));
-		TriangleVertices[i + 2] = 0;
+void GenerateSierpinskiPoints(int iterations) {
+    // Define the vertices of the main triangle
+    vec3 vertexA = vec3(-0.9f, -0.9f, 0.0f);
+    vec3 vertexB = vec3(0.9f, -0.9f, 0.0f);
+    vec3 vertexC = vec3(0.0f, 0.9f, 0.0f);
 
+    // Starting point (can be any random point inside the triangle)
+    vec3 currentPoint = vec3(0.0f, 0.0f, 0.0f);
 
-	}
+    // Add points using the Chaos Game
+    for (int i = 0; i < iterations; ++i) {
+        int randomVertex = rand() % 3;
 
-	// create buffer object
-	glGenBuffers(1, &VBO);
+        // Move halfway towards a random vertex
+        if (randomVertex == 0) {
+            currentPoint = (currentPoint + vertexA) * 0.5f;
+        }
+        else if (randomVertex == 1) {
+            currentPoint = (currentPoint + vertexB) * 0.5f;
+        }
+        else {
+            currentPoint = (currentPoint + vertexC) * 0.5f;
+        }
 
-	// binding buffer object
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(TriangleVertices), TriangleVertices, GL_STATIC_DRAW);
-	
-	// shader
-	glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
-	glEnableVertexAttribArray(0);
+        // Store the point
+        positions.push_back(currentPoint);
+    }
 
-	//GLuint Vertex_Position_Location = glGetAttribLocation(BasiceprogramId, "vertex_position");
+    // Update the VBO
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vec3) * positions.size(), positions.data(), GL_DYNAMIC_DRAW);
 }
 
-void CreateColoredTriangle()
-{
-	GLfloat TriangleVertices[] =
-	{
-		-1,-1,0,0,1,0,
-		1,-1,0,0,0,1,
-		0,1,0,1,0,0
-	};
+void Drawing() {
+    glClearColor(1.0, 1.0, 1.0, 0.0);
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
-	// create buffer object
-	glGenBuffers(1, &VBO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(vec3), nullptr);
+    glEnableVertexAttribArray(0);
 
-	// binding buffer object
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(TriangleVertices), TriangleVertices, GL_STATIC_DRAW);
-
-	// shader
-	glVertexAttribPointer(0, 3, GL_FLOAT, false, 6 * sizeof(GL_FLOAT), 0);
-	glEnableVertexAttribArray(0);
-
-	glVertexAttribPointer(1, 3, GL_FLOAT, false, 6 * sizeof(GL_FLOAT), (char*)(3 * sizeof(GL_FLOAT)));
-	glEnableVertexAttribArray(1);
+    GenerateSierpinskiPoints(9000);
 }
 
-void CompileShader(const char* vertex_shader_file_name, const char* fragment_shader_file_namering, GLuint& programId)
-{
-	programId = InitShader(vertex_shader_file_name, fragment_shader_file_namering);
-	glUseProgram(programId);
+int Init() {
+    GLenum err = glewInit();
+    if (err != GLEW_OK) {
+        cout << "Error";
+        getchar();
+        return 1;
+    }
+    else {
+        if (GLEW_VERSION_3_0)
+            cout << "Driver support OpenGL 3.0\nDetails:\n";
+    }
+    cout << "\tUsing glew " << glewGetString(GLEW_VERSION) << endl;
+    cout << "\tVendor: " << glGetString(GL_VENDOR) << endl;
+    cout << "\tRenderer: " << glGetString(GL_RENDERER) << endl;
+    cout << "\tVersion: " << glGetString(GL_VERSION) << endl;
+    cout << "\tGLSL:" << glGetString(GL_SHADING_LANGUAGE_VERSION) << endl;
+
+    Drawing();
+    glClearColor(0, 0.5, 0.5, 1);
+    return 0;
 }
 
-int Init()
-{
-	GLenum err = glewInit();
-	if (err != GLEW_OK)
-	{
-		cout << "Error";
-		getchar();
-		return 1;
-	}
-	else
-	{
-		if (GLEW_VERSION_3_0)
-			cout << "Driver support OpenGL 3.0\nDetails:\n";
-	}
-	cout << "\tUsing glew " << glewGetString(GLEW_VERSION) << endl;
-	cout << "\tVendor: " << glGetString(GL_VENDOR) << endl;
-	cout << "\tRenderer: " << glGetString(GL_RENDERER) << endl;
-	cout << "\tVersion: " << glGetString(GL_VERSION) << endl;
-	cout << "\tGLSL:" << glGetString(GL_SHADING_LANGUAGE_VERSION) << endl;
-
-	CompileShader("VS.glsl", "FS.glsl", BasiceprogramId);
-	CreateTriangle();
-	//CreateColoredTriangle();
-
-	glClearColor(0, 0.5, 0.5, 1);
-
-	return 0;
+void Render() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glDrawArrays(GL_POINTS, 0, positions.size());
 }
 
-void Render()
-{
-	glClear(GL_COLOR_BUFFER_BIT);
+int main() {
+    sf::ContextSettings context;
+    context.depthBits = 24;
+    sf::Window window(sf::VideoMode(WIDTH, HEIGHT), "Sierpinski's Triangle", sf::Style::Close, context);
 
-	switch (Current_DrawingMode)
-	{
-	case Points:
-		glPointSize(10);
-		glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
-		break;
-	case Lines:
-		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-		break;
-	case FilledTriangle:
-		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-		break;
-	default:
-		break;
-	}
+    if (Init()) return 1;
 
-	glDrawArrays(GL_TRIANGLE_FAN, 0, triangleSize);
-}
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
 
-float theta = 0;
-void Update()
-{
-	// add all tick code
-	theta += 0.0001f;
+        Render();
+        window.display();
+    }
 
-	GLuint Theta_Location = glGetUniformLocation(BasiceprogramId, "theta");
-	glUniform1f(Theta_Location, theta);
-}
-
-int main()
-{
-	sf::ContextSettings context;
-	context.depthBits = 24;
-	sf::Window window(sf::VideoMode(WIDTH, HEIGHT), "Computer Graphics", sf::Style::Close, context);
-
-	if (Init()) return 1;
-
-	while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			switch (event.type)
-			{
-			case sf::Event::Closed:
-			{
-				window.close();
-				break;
-			}
-			case sf::Event::KeyPressed:
-			{
-				if (event.key.code == sf::Keyboard::Num1)
-				{
-					Current_DrawingMode = DrawingMode::Points;
-				}
-				if (event.key.code == sf::Keyboard::Num2)
-				{
-					Current_DrawingMode = DrawingMode::Lines;
-				}
-				if (event.key.code == sf::Keyboard::Num3)
-				{
-					Current_DrawingMode = DrawingMode::FilledTriangle;
-				}
-				break;
-			}
-			}
-		}
-
-		//Update();
-		Render();
-
-		window.display();
-	}
-	return 0;
+    return 0;
 }
